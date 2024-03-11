a1f10e3b22a831dd306830fea90f68b1d97cb0c8
wu293 <156913364+wu293@users.noreply.github.com>
2024-03-12 01:59:48 +0800

    Update variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -5,3 +5,9 @@ if d<e:print("the two training makes better improvement")
 X=True
 Y=False
 W=X or Y
+# Truth table
+# X    Y    W
+# True  True  True
+# True  False  True
+# False True  True
+# False False False
