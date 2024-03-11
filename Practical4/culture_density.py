21f4520b8a0b9e7eb9decb8af950c6c483b83cae
wu293 <156913364+wu293@users.noreply.github.com>
2024-03-12 01:50:34 +0800

    Update culture_density.py

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,1 +1,7 @@
-
+density=5 #the original density is 5%
+day=1 #the initial day is marked as day1
+while density<=90: #Once the density passes 90% the cells will die
+    density=2*density 
+    day+=1 #This cell line doubles in density every 24 hrs
+print("I can stay away lab for",day,"days")
+print("On day",day,"the cell density is larger than 90%")
