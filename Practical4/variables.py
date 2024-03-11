fdaa32d268392f73fa9557dbac4aaf3dd93152f2
wu293 <156913364+wu293@users.noreply.github.com>
2024-03-12 02:03:06 +0800

    Update variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,7 +1,11 @@
+a=40
+b=36
+c=a-b
 d=a-b
 e=b-c
 if d>e:print("running training makes better improvement")
 if d<e:print("the two training makes better improvement")
+else:print("only running training's effect equal to the two training")
 X=True
 Y=False
 W=X or Y
