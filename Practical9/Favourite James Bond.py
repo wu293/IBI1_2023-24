88fb1aabd5d2a83d3d5606d31048ace37de61df4
wu293 <156913364+wu293@users.noreply.github.com>
2024-05-20 17:46:47 +0800

    Update Favourite James Bond.py

--- Practical9/Favourite James Bond.py
+++ Practical9/Favourite James Bond.py
@@ -1,1 +1,15 @@
+age = int(input("The year you were born is \n"))
+year= age+18
+def Favourite_JamesBond_Actor(year):
+    if year >= 1973 and year <= 1986:
+        print("Your favorite James Bond actor is Roger Moore")
+    elif year >= 1987 and year <= 1994:
+        print("Your favorite James Bond actor is Timothy Dalton")
+    elif year >= 1995 and year <= 2005:
+        print("Your favorite James Bond actor is Pierce Brosnan")
+    elif year >= 2006 and year <= 2021:
+        print("Your favorite James Bond actor is aniel Craig")
+    else:
+        print("error")
 
+Favourite_JamesBond_Actor(year)
