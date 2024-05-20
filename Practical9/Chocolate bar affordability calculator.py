95d19d4fa89fc0d72b8ba206a495cf4c16cbdb62
wu293 <156913364+wu293@users.noreply.github.com>
2024-05-20 17:45:38 +0800

    Update Chocolate bar affordability calculator.py

--- Practical9/Chocolate bar affordability calculator.py
+++ Practical9/Chocolate bar affordability calculator.py
@@ -1,1 +1,10 @@
+def chocolatebar_affordability(money, price_perbar):
+    number_bars = money // price_perbar
+    change_left = money % price_perbar
+    return number_bars, change_left
 
+# example
+money = 199
+price_perbar = 6
+bars_bought, change = chocolatebar_affordability(money, price_perbar)
+print(f"With ${money}, you can buy {bars_bought} chocolate bars and you will have ${change} left.")
