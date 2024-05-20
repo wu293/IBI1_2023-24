9ee8afd58d906c8ba8ae3bcc448ca64e66fff6f7
wu293 <156913364+wu293@users.noreply.github.com>
2024-05-20 21:09:58 +0800

    Update SIR.py

--- Practical10/SIR.py
+++ Practical10/SIR.py
@@ -1,1 +1,34 @@
+mport numpy as np  
+import matplotlib.pyplot as plt  
 
+N=1000
+I=1
+R=0
+S=N-I-R
+beta = 0.3
+gamma =0.05
+
+S_array = np.zeros(1000)
+I_array = np.zeros(1000)
+R_array = np.zeros(1000)
+
+S_array[0] = S
+I_array[0] = I
+R_array[0] = R
+
+for t in range(1, 1000):  
+    Newly_infected_people = np.random.binomial(S_array[t-1], beta * I_array[t-1] / N)  
+    New_recovered_people = np.random.binomial(I_array[t-1], gamma)  
+    S_array[t] = S_array[t-1] - Newly_infected_people  
+    I_array[t] = I_array[t-1] + Newly_infected_people - New_recovered_people  
+    R_array[t] = R_array[t-1] + New_recovered_people  
+  
+plt.plot(S_array, label='Susceptible')  
+plt.plot(I_array, label='Infected')  
+plt.plot(R_array, label='Recovered')  
+plt.xlabel('Time')
+plt.ylabel('Number')
+plt.title('SIR Model')
+plt.legend()  
+plt.show()
+plt.clf
