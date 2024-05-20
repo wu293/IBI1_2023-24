37c86b9c4a9bbcf8e89705281931256ce31467b0
wu293 <156913364+wu293@users.noreply.github.com>
2024-05-20 20:33:55 +0800

    Update get_duplicate_genes.py

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,1 +1,24 @@
+import os
+import re
+os.chdir("/Users/yun/Desktop/IBInotes")
+input_file = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
+output_file = open('duplicate_genes.fa', 'w')
+sequence=[]
+flag=1
+for line in input_file:
+    line = line.strip()
+    if line.startswith('>'):
+        if sequence and flag:
+                output_file.write(''.join(sequence)+'\n')
+                sequence=[]
+        flag=1
+        if re.search('duplication', line):
+                genename_match=re.search(r'gene:(\w+)',line)
+                if genename_match:
+                    output_file.write(f'>{genename_match.group(1)}\n')
+                    flag=0
+        else:
+                    sequence.append(line)
 
+        if sequence and flag:
+            output_file.write(''.join(sequence)+'\n')
