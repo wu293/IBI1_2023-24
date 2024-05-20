1acbc3e0c5b63e63f7656f7cc6132c8142e42990
wu293 <156913364+wu293@users.noreply.github.com>
2024-05-20 21:05:27 +0800

    Update count_repeat_duplicate_genes.py

--- Practical8/count_repeat_duplicate_genes.py
+++ Practical8/count_repeat_duplicate_genes.py
@@ -1,1 +1,37 @@
+import re
 
+def count_repeat_duplicate_genes(inputfile, outputfile):
+    with open(inputfile, 'r') as input_fasta, open(outputfile, 'w') as output_fasta:
+        sequence = []
+        repeat_pattern = re.compile(r'(GTGTGT|GTCTGT)')  
+        flag=1
+
+        for line in input_fasta:
+            line = line.strip()
+            if line.startswith('>'):
+                if sequence and flag:
+                        output_fasta.write(''.join(sequence) + '\n')
+                        sequence = []
+                        flag=1
+               
+                if re.search('duplication', line):
+                    genename_match = re.search(r'gene:(\w+)', line)
+                    if genename_match:
+                        gene_name = genename_match.group(1)                        
+                        repeat_count = len(repeat_pattern.findall(''.join(sequence)))                    
+                        new_gene_name = f"{gene_name}_{repeat_count}"
+                        output_fasta.write(f">{new_gene_name}\n")
+                        flag=0
+            else:
+                sequence.append(line)
+
+        if sequence and flag:
+            output_fasta.write(''.join(sequence) + '\n')
+
+# Prompts the user enter a repeat sequence
+repeat_sequence = input("Please enter one of the repetitive sequences ('GTGTGT' or 'GTCTGT'): ")
+if repeat_sequence not in ['GTGTGT', 'GTCTGT']:
+    print("Invalid sequence entered.")
+else:
+    output_filename = f"{repeat_sequence}_duplicate_genes.fa"
+    count_repeat_duplicate_genes('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', output_filename)
